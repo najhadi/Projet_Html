9f30a4a038fa4d7a0f7478568df4e6ede6456a01
najhadi <najoua_14@hotmail.fr>
2022-06-06 23:11:03 -0400

    Ajout Attribut Depense

--- src/modele/Depense.java
+++ src/modele/Depense.java
@@ -9,5 +9,44 @@ package modele;
  * @author Utilisateur
  */
 public class Depense {
+    private double frais;
+    private double max;
+    private String type;
+
+    public Depense() {
+    }
+       public Depense(double frais, double max, String type) {
+        this.frais = frais;
+        this.max = max;
+        this.type = type;
+    }
+
+    public void setFrais(double frais) {
+        this.frais = frais;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getFrais() {
+        return frais;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+ 
+    
+    
     
 }
