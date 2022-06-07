67c9787a58841c26d31c0491d5f77508e6beabc6
najhadi <najoua_14@hotmail.fr>
2022-06-06 22:54:34 -0400

    creation classes

--- src/modele/Commercial.java
+++ src/modele/Commercial.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package test;
+package modele;
 
 /**
  *
  * @author Utilisateur
  */
-public class Test {
+public class Commercial {
 
     /**
      * @param args the command line arguments
